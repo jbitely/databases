a60bff957ac0a9bdce359be65d8340446ceb6dd7
Justin Bitely <jbitely@gmail.com>
2016-02-05 20:37:00 -0500

    Implemented schema and created tables.

--- server/schema.sql
+++ server/schema.sql
@@ -4,10 +4,26 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  message_id INTEGER(10) NOT NULL AUTO_INCREMENT,
+  content VARCHAR(3000) NULL,
+  user_id INTEGER(10) NOT NULL DEFAULT 1,
+  room_id INTEGER(10) NOT NULL DEFAULT 1,
+  PRIMARY KEY (message_id)
+);
+/* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  user_id INTEGER(10) NOT NULL AUTO_INCREMENT,
+  username VARCHAR(20) NOT NULL DEFAULT 'Anonymous',
+  PRIMARY KEY (user_id),
+  CONSTRAINT unique_name UNIQUE (username)
 );
 
-/* Create other tables and define schemas for them here! */
-
+CREATE TABLE rooms (
+  room_id INTEGER(10) NOT NULL AUTO_INCREMENT,
+  roomname VARCHAR(20) NOT NULL DEFAULT 'Lobby',
+  PRIMARY KEY (room_id),
+  CONSTRAINT unique_name UNIQUE (roomname)
+);
 
 
 
